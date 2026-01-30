e89fbe9af8f3614f5281a920a27e5a9d7c7f20e2
Ahmed-Adel-Ramadan <ahmedadelprogramming@gmail.com>
2026-01-30 22:36:43 +0200

    Add Structure

---
+++ Solutions/Implementation/Ch02/Ch02_MergeSort.c
@@ -0,0 +1,57 @@
+/*
+    Chapter:      02 - Getting Started
+    Algorithm:    Merge Sort (Divide and Conquer)
+    Contributor:  [Ahmed Adel / Ahmed-Adel-Ramadan]
+    Complexity:   theta(n log n)
+    --------------------------------------------------------------------
+    Description: 
+        Merge Sort is a Divide and Conquer algorithm that divides the input array into two halves,
+        calls itself for the two halves, and then merges the two sorted halves.
+    --------------------------------------------------------------------
+    
+*/
+#include <stdio.h>
+
+void merge_sort(int arr[], int length);
+
+int main(void) {
+    printf("Enter The Number Of Elements: ");
+    int length;
+    scanf("%i", &length);
+
+    int arr[length];
+    for (int i = 0; i < length; ++i) {
+        printf("Enter Element %i: ", i + 1);
+        scanf("%i", arr + i);
+    }
+
+    merge_sort(arr, length);
+
+    for (int i = 0; i < length; ++i) {
+        printf("%i ", arr[i]);
+    }
+}
+
+void merge_sort(int arr[], int length) {
+    if (length <= 1) {
+        return;
+    }
+
+    // Sort Left Half
+    merge_sort(arr, length / 2);
+    // Sort Right Half
+    merge_sort(arr + length / 2, length - length / 2);
+
+    int merged[length];
+    int l = 0, r = length / 2;
+    for (int i = 0; i < length; ++i) {
+        if (l < length / 2 && (r == length || arr[l] <= arr[r])) {
+            merged[i] = arr[l++];
+        } else {
+            merged[i] = arr[r++];
+        }
+    }
+
+    for (int i = 0; i < length; ++i)
+        arr[i] = merged[i];
+}
